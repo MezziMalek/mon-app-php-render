591f2ebd3d52f08684955cf2336960c06528c6e8
Malek <malekm12@gmail.com>
2025-12-11 12:03:14 +0000

    Initial commit - Application PHP pour Render

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php 
+echo "<h1>Bienvenue sur mon application PHP déployée avec Rnder !</h1>";
+echo "<p>Date et Heure :" .date('Y-m-d H:i:s') ."</p>";
+echo "<p> Version PHP: ". phpversion() . "</p>";
+echo "<hr>";
+echo "<h2> Informations du serveur:</h2>";
+echo "<p>Systéme : " . php_uname() . "</p>";
+?>
+
